1bf3b80ddaed098f58dc5731233bce9c3ffa94ec
Aidan Chamberlain <asunshine99@gmail.com>
2022-02-12 12:58:51 -0600

    update to letter.js to pass test cases

--- week10/lib/Letter.js
+++ week10/lib/Letter.js
@@ -1,5 +1,16 @@
 class Letter {
-  
+    constructor(char) {
+        this.visible = !/[a-z0-9]/i.test(char);
+        this.char = char;
+    }
+
+    guess(letter) {
+        if (this.char === letter) {
+            this.visible = true;
+            return true;
+        }
+        return false;
+    }
 }
 
 module.exports = Letter;
