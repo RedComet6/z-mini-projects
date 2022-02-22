8b49cae650fe3395004526916643b57d74fadda1
Aidan Chamberlain <asunshine99@gmail.com>
2022-02-21 22:24:54 -0600

    diagnostics get and post completed

--- week11/routes/index.js
+++ week11/routes/index.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 // Import our modular routers for /tips and /feedback
-const tipsRouter = require('./tips');
-const feedbackRouter = require('./feedback');
-// TODO: import your diagnostics route
+const tipsRouter = require("./tips");
+const feedbackRouter = require("./feedback");
 
+// TODO: import your diagnostics route
+const diagnosticRouter = require("./diagnostics");
 const app = express();
 
-app.use('/tips', tipsRouter);
-app.use('/feedback', feedbackRouter);
+app.use("/tips", tipsRouter);
+app.use("/feedback", feedbackRouter);
+
 // TODO: Initialize diagnostics route
+app.use("/diagnostics", diagnosticRouter);
 
 module.exports = app;
